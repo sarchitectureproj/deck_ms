1b34b125f95073ecc7df677f5b4bf1e373349253
nicolasprr <niparrara@unal.edu.co>
2019-05-31 19:08:40 -0500

    seed it's working

--- src/routes/cabins.routes.js
+++ src/routes/cabins.routes.js
@@ -7,7 +7,7 @@ function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 function getRandomClass() {
-    const choose = Math.floor.Math.random() * (4);
+    const choose = Math.floor(Math.random() * (3));
     const categories = ["Premiun business", "Premiun economy", "Economy"];
     return categories[choose]
 }
@@ -45,35 +45,38 @@ router.get('/busy', async (req, res) => {
 })
 router.get('/seed', async (req, res) => {
     const db = req.app.locals.database;
-    let id;
+    const query = { "passangers": { $gte: "1" } }
     let data;
     try {
         for (let i = 0; i < 10; i++) {
             //create deck
             data = {
                 floor: (i + 1),
+                meeting_schedule: null,
+                meeting_point_id: null,
             }
-            const result = await db.colletion("decks").insert(data);
-            id = result.ops[0]._id;
-
+            const db = req.app.locals.database;
+            const result = await db.collection('decks').insert(data);
+            console.log(result)
+            const id = result.ops[0]._id
             for (let j = 0; j < 100; j++) {
                 data = {
                     capacity: getRandomArbitrary(1, 5),
                     category: getRandomClass(),
-                    passangers: 0,
                     deck_id: ObjectID(id),
                     position: getPosition(i + 1, j + 1),
                 }
-                await db.collection(collection).insert(data);
+                const rs = await db.collection(collection).insert(data);
             }
         }
         const result = await db.collection(collection).find({}).toArray();
-        res.status(200).json(result)
+        res.json(result);
     } catch (error) {
+        console.log(error)
         res.status(500).json({ error: error.toString() });
     }
-
 })
+
 
 router.post('/', async (req, res) => {
     const db = req.app.locals.database;
